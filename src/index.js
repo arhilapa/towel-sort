46d4b36bb157852af102927bb5f852ed10664868
Arhilapa <arhilapa1@gmail.com>
2022-08-12 22:03:31 +0300

    fead: task complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-module.exports = function towelSort (matrix) {
-  result = [];
-  if (matrix == undefined) {return []}
-  else {matrix.forEach((element, index) => {if (index % 2 === 0) {result[index] = element}
-  else {result[index] = element.reverse()}});
-  return result.flat()}
-}
+module.exports = function towelSort(matrix) {
+    result = [];
+    if (matrix == undefined) {
+        return [];
+    } else {
+        matrix.forEach((element, index) => {
+            if (index % 2 === 0) {
+                result[index] = element;
+            } else {
+                result[index] = element.reverse();
+            }
+        });
+        return result.flat();
+    }
+};
